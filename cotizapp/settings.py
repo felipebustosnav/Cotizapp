e712a43424f2115f3fba1419576fdf0006bc7a0d
CotizApp Developer <dev@cotizapp.local>
2025-12-09 18:09:55 -0300

    Activar URLs firmadas para S3

--- cotizapp/settings.py
+++ cotizapp/settings.py
@@ -178,7 +178,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
     AWS_S3_VERIFY = True
-    AWS_QUERYSTRING_AUTH = False
+    
     
     # Static and Media file storage
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
